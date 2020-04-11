9e4386d975a1dffb4773f434562aa22fbef57f79
unknown <schlappa>
2020-04-11 19:41:42 -0400

    add canvas

---
+++ src/script.js
@@ -0,0 +1,9 @@
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+// configuration
+context.font = "30px Arial";
+context.color = "#FFF";
+
+// test
+context.fillText("Hello World", 10, 50);
