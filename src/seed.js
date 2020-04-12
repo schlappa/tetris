8bb00fe46bd11fa8a51734a45a5ecc34e42bc938
unknown <schlappa>
2020-04-12 19:39:35 -0400

    remove color properties

--- src/seed.js
+++ src/seed.js
@@ -1,4 +1,4 @@
-[
+export let pieces = [
     {
         name: "I",
         matrix: [
@@ -6,40 +6,35 @@
             [1],
             [1],
             [1]
-        ],
-        color: "red"
+        ]
     },
     {
         name: "O",
         matrix: [
             [2, 2],
             [2, 2]
-        ],
-        color: "blue"
+        ]
     },
     {
         name: "T",
         matrix: [
             [3, 3, 3],
             [0, 3, 0]
-        ],
-        color: "brown"
+        ]
     },
     {
         name: "S",
         matrix: [
             [0, 4, 4],
             [4, 4, 0]
-        ],
-        color: "green"
+        ]
     },
     {
         name: "Z",
         matrix: [
             [5, 5, 0],
             [0, 5, 5]
-        ],
-        color: "cyan"
+        ]
     },
     {
         name: "J",
@@ -47,15 +42,14 @@
             [0, 6],
             [0, 6],
             [6, 6]
-        ],
-        color: "white"
+        ]
     },
     {
         name: "L",
         matrix: [
             [7, 0],
             [7, 0],
-        ],
-        color: "magenta"
+            [7, 7]
+        ]
     }
 ]
