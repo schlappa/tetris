0219f152efb0b68641ed3369669a8028d927f95c
unknown <schlappa>
2020-04-12 18:33:38 -0400

    add seed.js

---
+++ src/seed.js
@@ -0,0 +1,62 @@
+[
+    {
+        piece: "I",
+        matrix: [
+            [1],
+            [1],
+            [1],
+            [1]
+        ],
+        color: "red"
+    },
+    {
+        piece: "O",
+        matrix: [
+            [1,1],
+            [1,1]
+        ],
+        color: "blue"
+    },
+    {
+        piece: "T",
+        matrix: [
+            [1,1,1],
+            [0,1,0]
+        ],
+        color: "brown"
+    },
+    {
+        piece: "S",
+        matrix: [
+            [0,1,1],
+            [1,1,0]
+        ],
+        color: "green"
+    },
+    {
+        piece: "Z",
+        matrix: [
+            [1,1,0],
+            [0,1,1]
+        ],
+        color: "cyan"
+    },
+    {
+        piece: "J",
+        matrix: [
+            [0,1],
+            [0,1],
+            [1,1]
+        ],
+        color: "white"
+    },
+    {
+        piece: "L",
+        matrix: [
+            [1,0],
+            [1,0],
+            [1,1]
+        ],
+        color: "magenta"
+    }
+]
