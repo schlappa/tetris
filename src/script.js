6c91a0e545d2dc7b38bbddd06c07c31a1e627fb4
unknown <schlappa>
2020-04-11 22:04:16 -0400

    add a matrix

--- src/script.js
+++ src/script.js
@@ -1,16 +1,27 @@
 let context
+let matrix;
 
 function init() {
-    var canvas = document.getElementById("canvas")
+    // TODO: the parameters should be replaced for user input values     
+    initCanvas("canvas");
+    initMatrix(6, 10);
+}
+
+function initCanvas(canvasId) {
+    var canvas = document.getElementById(canvasId)
     
     context = canvas.getContext("2d")
     context.canvas.width = window.innerWidth
     context.canvas.height = window.innerHeight
 }
 
-function draw() {
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
+function initMatrix(height, width) {
+    matrix = Array(height).fill(Array(width).fill(0));
+}
+
+function drawMatrix() {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
             drawSquare({x: i * 40, y: j * 40}, 40, 4, "yellow")
         }
     }
@@ -25,5 +36,7 @@ function drawSquare(position, side, padding, color) {
         side - padding)
 }
 
+
+
 init()
-draw()
+drawMatrix()
