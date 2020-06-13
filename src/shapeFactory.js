c2d714b3a70602863dda80ce92ad8e67c8b32e30
unknown <schlappa>
2020-06-13 18:27:30 -0400

    simplify code

--- src/shapeFactory.js
+++ src/shapeFactory.js
@@ -7,30 +7,33 @@ export function createShape(matrix, shape) {
     const shapeInstance = {
         guid: getTimestamp(),
         shape: shape,
-        squares: createSquares(matrix, shape.matrix, startPosition)
+        squares: createSquares(matrix, shape, startPosition)
     };
 
     return shapeInstance;
 }
 
-function createSquares(matrix, subMatrix, startPosition) {
+function createSquares(matrix, shape, startPosition) {
+    const subMatrix = shape.matrix
+
     const [height, width] = getEdgeSizes(subMatrix)
     const squares = []
 
     for (let x = 0; x < height; x++) {
         for (let y = 0; y < width; y++) {
-            const matrixCellIsEmpty = matrix[startPosition.x + x][startPosition.y + y].square === undefined
+            const matrixCell = getMatrixCellByPosition(matrix, startPosition.x + x, startPosition.y + y)
+            const matrixCellIsEmpty = matrixCell.square === undefined
             const subMatrixIsNotEmpty = subMatrix[x][y] != 0
 
             if (matrixCellIsEmpty && subMatrixIsNotEmpty) {
                 const matrixPosition = { x: startPosition.x + x, y: startPosition.y + y }
                 const subMatrixPosition = { x: x, y: y }
+                const color = shape.color
                 const square = createSquare(
                     matrix,
                     matrixPosition,
                     subMatrixPosition,
-                    getTimestamp(),
-                    getColor(subMatrix[x][y]))
+                    color)
 
                 squares.push(square)
             }
@@ -38,28 +41,17 @@ function createSquares(matrix, subMatrix, startPosition) {
     }
 }
 
-function createSquare(matrix, matrixPosition, subMatrixPosition, timestamp, color) {
+function createSquare(matrix, matrixPosition, subMatrixPosition, color) {
     const square = {
-        guid: timestamp,
+        guid: getTimestamp(),
         matrixPosition: matrixPosition,
         subMatrixPosition: subMatrixPosition,
         color: color
     }
+    
     matrix[matrixPosition.x][matrixPosition.y].square = square
 
     return square
-}
-
-function getColor(value) {
-    switch (value) {
-        case 1: return "red"
-        case 2: return "blue"
-        case 3: return "brown"
-        case 4: return "green"
-        case 5: return "cyan"
-        case 6: return "white"
-        case 7: return "magenta"
-    }
 }
 
 export function increaseGravityForShape(matrix, shapeInstance) {
--- src/shapes.js
+++ src/shapes.js
@@ -1,51 +1,52 @@
 export let shapes = [
     {
         name: "I",
+        color: "red",
         matrix: [
             [1],
             [1],
             [1],
             [1]
         ]
-    },
-    {
+    }, {
         name: "O",
+        color: "blue",
         matrix: [
             [2, 2],
             [2, 2]
         ]
-    },
-    {
+    }, {
         name: "T",
+        color: "brown",
         matrix: [
             [3, 3, 3],
             [0, 3, 0]
         ]
-    },
-    {
+    }, {
         name: "S",
+        color: "green",
         matrix: [
             [0, 4, 4],
             [4, 4, 0]
         ]
-    },
-    {
+    }, {
         name: "Z",
+        color: "cyan",
         matrix: [
             [5, 5, 0],
             [0, 5, 5]
         ]
-    },
-    {
+    }, {
         name: "J",
+        color: "white",
         matrix: [
             [0, 6],
             [0, 6],
             [6, 6]
         ]
-    },
-    {
+    }, {
         name: "L",
+        color: "magenta",
         matrix: [
             [7, 0],
             [7, 0],
